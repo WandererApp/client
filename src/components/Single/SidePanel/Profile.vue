279dcbf951339f0ec5e02276d97bd37c53ac6072
MrTheGood <maarten@degoedeweb.nl>
2019-12-07 19:54:57 +0100

    Add profile

---
+++ src/components/Single/SidePanel/Profile.vue
@@ -0,0 +1,66 @@
+<template>
+    <div id="container">
+
+        <div id="person">
+            <div id="profilePhoto"><img src="img/background_authenticate.png"/></div>
+            <h3>Kevin von Kevinson</h3>
+            <p>Joined 5 october 2017</p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "Profile",
+        components: {},
+    }
+</script>
+
+<style scoped>
+    #container {
+        width: 100%;
+    }
+
+    #container #person {
+        border-bottom: 1px #c4c4c4 solid;
+
+        padding-bottom: 40px;
+    }
+
+    #container #person #profilePhoto {
+        display: flex;
+        justify-content: center;
+
+        width: 100%;
+    }
+
+    #container #person #profilePhoto img {
+        width: 176px;
+        height: 176px;
+        border-radius: 50%;
+
+        margin-top: 24px;
+    }
+
+    #container #person h3 {
+        margin-top: 8px;
+        text-align: center;
+
+        font-weight: normal;
+    }
+
+    #container #person p {
+        text-align: center;
+        font-size: 12px;
+    }
+
+    @media (min-width: 1023px) {
+        #container {
+            width: 400px;
+            flex-shrink: 0;
+            flex-grow: 0;
+            border-right: 1px solid #e4e4e4;
+        }
+    }
+</style>
---
+++ src/components/Single/SidePanel/Timeline.vue
@@ -0,0 +1,69 @@
+<template>
+    <div id="container">
+        <div class="search">
+            <SearchInput type="text" placeholder="Find wanderers"></SearchInput>
+        </div>
+        <div class="timeline">
+            <div class="item" data-id="1">
+                <HorizontalProfileView/>
+                <div class="content">
+                    We hebben een geweldige trip gehad naar het buitenland. <br>
+                    Zeker een aanrader.
+                </div>
+            </div>
+            <div class="item" data-id="2">
+                <HorizontalProfileView/>
+                <div class="content">
+                    We hebben een geweldige trip gehad naar het buitenland. <br>
+                    Zeker een aanrader.
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import SearchInput from "../../Multi/Input/SearchInput";
+    import HorizontalProfileView from "../../Multi/profile/HorizontalProfileView"
+
+    export default {
+        name: "Timeline",
+        components: {HorizontalProfileView, SearchInput},
+    }
+</script>
+
+<style scoped>
+    #container {
+        width: 100%;
+        padding-left: 15px;
+        padding-right: 15px;
+        padding-top: 15px;
+    }
+
+    #container .search {
+        width: 100%;
+    }
+
+    #container .timeline {
+        width: 100%;
+        margin-top: 15px;
+    }
+
+    #container .timeline .item {
+        margin-bottom: 30px;
+    }
+
+    #container .timeline .item .content {
+        font-size: 14px;
+        margin-top: 10px;
+    }
+
+    @media (min-width: 1023px) {
+        #container {
+            width: 400px;
+            flex-shrink: 0;
+            flex-grow: 0;
+            border-right: 1px solid #e4e4e4;
+        }
+    }
+</style>
